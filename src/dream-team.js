b153242dc8d8df4367c087d16a5cda5c221e3705
VikaKrasnogir <vikakrasnogir269@gmail.com>
2022-10-17 17:21:04 +0300

    feat: add result

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (typeof members !== 'string' && !Array.isArray(members)) { return false }
+  //оставляем в массиве только строки
+  const onlyStringMembersFirstLetter = members.filter(item => typeof item === 'string');
+  //убираем из строчных элементов массива пробелы
+  const noSpace = onlyStringMembersFirstLetter.map(item => item.replace(/ /g, ''))
+  //сортируем буквы в алфавитном порядке
+  const firstLetter = noSpace.map(item => item[0].toUpperCase()).sort((a, b) => {
+    return a.localeCompare(b);
+  })
+  //получаем строку - результат
+  const result = firstLetter.join('')
+  return result;
 }
 
 module.exports = {
